1ffa4fc690010e813076edb0eabee86081b4d154
Alvaro Rocha <alvarorocha@protonmail.com>
2023-11-23 19:01:33 -0300

    Exemplo de HashMap

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -46,6 +46,26 @@ public class PrincipalComListas {
         lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
         System.out.println("Ordenando por Ano: " + lista);
 
+        //ExemploHashMap
+        Map<String, Integer> usandoHashMap = new HashMap<>();
+
+        // Adicionando pares chave-valor
+        usandoHashMap.put("Gatos", 1);
+        usandoHashMap.put("Cachorros", 2);
+        usandoHashMap.put("Roedores", 3);
+
+        // Acessando um valor através de uma chave
+        int valor = usandoHashMap.get("Cachorros");
+        System.out.println("Valor da chave Cachorros: " + valor);
+
+        // Removendo um par chave-valor
+        usandoHashMap.remove("Gatos");
+
+        // Iterando sobre as chaves
+        for (String chave : usandoHashMap.keySet()) {
+            System.out.println("Chave: " + chave);
+            System.out.println("Valor: " + usandoHashMap.get(chave));
+        }
     }
 
 }
